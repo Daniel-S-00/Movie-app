81effc5e7fb9469badcdb23fffdcb3b86ab022ae
Daniel-S-00 <daniel.sebastian0407@gmail.com>
2025-11-26 18:51:43 -0600

    Added basic header container

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 function App() {
-  return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <main>
+      <div className="pattern"></div>
+      <div className="wrapper">
+        <header>
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Love
+          </h1>
+        </header>
+        <p>Search</p>
+      </div>
+    </main>
+  );
 }
 
 export default App;
