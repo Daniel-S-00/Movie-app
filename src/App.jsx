c86f06928d97c1b1c1e0706b3c7841a72a4b1c39
Daniel-S-00 <daniel.sebastian0407@gmail.com>
2025-12-05 00:24:42 -0600

    Adds search functionality to the application
    Introduces a search component allowing users to find movies.
    
    This includes:
    - A search input field that updates the search term as the user types.
    - Displaying the current search term.
    - Styling improvements with tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
-import React from "react";
-
+import { useState } from "react";
+import Search from "./components/search";
 function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+
   return (
     <main>
-      <div className="pattern"></div>
+      <div className="pattern" />
       <div className="wrapper">
         <header>
+          <img src="/hero.png" alt="Hero banner" />
           <h1>
             Find <span className="text-gradient">Movies</span> You'll Love
           </h1>
         </header>
-        <p>Search</p>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <h1 className="text-white">{searchTerm}</h1>
       </div>
     </main>
   );
