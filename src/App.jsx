9ab4d1ef4941cbaadf98274e01df9a935cbdfb0e
Daniel-S-00 <daniel.sebastian0407@gmail.com>
2025-12-08 17:42:53 -0600

    Adds MovieCard component
    Introduces a reusable MovieCard component to display movie information.
    
    Replaces the simple list items with the new MovieCard component in the main App component, improving the presentation of movie data.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
-import Search from "./components/search";
-import Spinner from "./components/spinner";
+import Search from "./components/Search";
+import Spinner from "./components/Spinner";
+import MovieCard from "./components/MovieCard";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -66,9 +67,7 @@ function App() {
           ) : (
             <ul>
               {movieList.map((movie) => (
-                <li className="text-white" key={movie.id}>
-                  {movie.title}
-                </li>
+                <MovieCard key={movie.id} movie={movie} />
               ))}
             </ul>
           )}
